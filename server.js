d30e622255573e778730fbb1c2fd04e514c2e9f0
dchicchon <danielchicchon@gmail.com>
2019-08-22 18:12:48 -0700

    add access-control

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const app = express();
 const mongoose = require("mongoose")
 const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI || "mongodb://localhost/zoo"
+const cors = require("cors")
 
 global.db = mongoose.createConnection(uri, { useNewUrlParser: true });
 
 const routes = require("./routes")
+
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next()
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
