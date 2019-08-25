afea65ba51936ed0e6f49d3c4e455a58e097be7d
dchicchon <danielchicchon@gmail.com>
2019-08-25 13:31:41 -0700

    add new scripts to server json

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
-const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI || "mongodb://localhost/zoo"
 const cors = require("cors")
 
@@ -28,6 +27,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', routes)
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Server listening on PORT:", PORT)
