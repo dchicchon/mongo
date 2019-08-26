7e5ca457bb0c782343c424be6b8d9d9a112012b2
dchicchon <danielchicchon@gmail.com>
2019-08-25 22:23:28 -0700

    add test message API

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -1,7 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
     getAnimal: function () {
+        console.log("HI")
         return axios.get('/api/animals')
     },
 
